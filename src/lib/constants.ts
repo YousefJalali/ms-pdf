3ebe80ea20faa10ad5fe72a184d218aa9739d00c
Yousef Jalali <yousefjalalidev@gmail.com>
2024-07-29 14:36:33 +0300

    update main page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,28 +6,25 @@ export const TOOLS = [
 		name: 'Merge PDF',
 		link: '/merge',
 		icon: MergeIcon,
-		description:
-			'Combine multiple PDF files into a single, organized document with our Merge Tool. Perfect for consolidating reports, presentations, or any other documents, this tool allows you to seamlessly join files, ensuring a smooth and efficient workflow.'
+		description: 'Combine multiple PDF files into a single, organized document'
 	},
 	{
 		name: 'Split PDF',
 		link: '/split',
 		icon: SplitIcon,
-		description:
-			'Easily separate a large PDF into individual pages or custom segments. Whether you need to extract a single page or divide a document into multiple sections.'
+		description: 'Easily separate a large PDF into individual pages'
 	},
 	{
 		name: 'PDF to Image',
 		link: '/pdf-to-image',
 		icon: ConvertToImageIcon,
-		description:
-			'Easily convert a large PDF into individual images (JPEG, PNG, WEBP...). Whether you need to extract a single page or convert an entire document into multiple high-quality images, our tool makes it simple and efficient.'
+		description: 'Easily convert a large PDF into individual images'
 	},
 	{
 		name: 'Compress PDF',
 		link: '/compress',
 		icon: CompressIcon,
 		description:
-			'Effortlessly compress your PDF files to reduce their size without compromising quality. Whether you need to send a document via email or save storage space, our tool ensures your PDFs remain clear and professional while taking up less room.'
+			'Effortlessly compress your PDF files to reduce their size without compromising quality'
 	}
 ]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,30 @@
 <script lang="ts">
 	import { TOOLS } from '$lib/constants'
-	import { onMount } from 'svelte'
+	// import { onMount } from 'svelte'
 
-	onMount(async () => {
-		await import('@lottiefiles/lottie-player')
-	})
+	// onMount(async () => {
+	// 	await import('@lottiefiles/lottie-player')
+	// })
+
+	function scrollToBottom() {
+		if (!window) return
+		window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+	}
 </script>
 
-<section class="flex items-center h-[60vh]">
-	<div class="prose">
-		<h1 class="text-5xl font-black">Transform PDFs Like a Pro</h1>
+<section class="flex items-center justify-center min-h-[calc(100vh-100px)]">
+	<div class="text-center prose prose-xl">
+		<h1>Transform PDFs Like a Pro</h1>
 		<p>
 			Step into a world where managing PDFs is as thrilling as a magician's trick! With our
 			cutting-edge app, you can merge and split PDFs effortlessly. Fuse documents into a single
 			masterpiece or break them apart with pinpoint accuracy. Turn your PDF tasks into a breeze and
 			unleash your productivity!
 		</p>
+		<button class="btn btn-primary" on:click={scrollToBottom}>Get Started</button>
 	</div>
 
-	<div class="w-2/3 min-w-[400px]">
+	<!-- <div class="w-2/3 min-w-[400px]">
 		<lottie-player
 			src="https://lottie.host/691a7996-25af-4446-9c6b-78d23cf930f1/uJjkoYOdpk.json"
 			background="#FFFFFF"
@@ -29,18 +35,15 @@
 			direction="1"
 			mode="normal"
 		></lottie-player>
-	</div>
+	</div> -->
 </section>
 
-<section class="my-24">
-	<h2 class="text-6xl font-black mb-8">Our Tools</h2>
+<section class="mb-24">
+	<h2 class="text-4xl font-black mb-8">Our Tools</h2>
 
-	<div class="flex flex-wrap gap-8">
+	<div class="grid grid-cols-4 gap-8">
 		{#each TOOLS as tool}
-			<a
-				href={tool.link}
-				class="prose min-w-96 flex-1 max-w-sm border rounded-2xl p-6 hover:bg-base-200"
-			>
+			<a href={tool.link} class="prose flex-1 max-w-sm border rounded-2xl p-6 hover:bg-base-200">
 				<span>{@html tool.icon}</span>
 				<h3>{tool.name}</h3>
 				<p>{tool.description}</p>
