bfc40581f5f5db3cc412e69d6196ed4913ccb292
Yousef Jalali <yousefjalalidev@gmail.com>
2024-07-30 13:22:18 +0300

    fix home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,31 +7,33 @@
 	}
 </script>
 
-<section class="flex items-center justify-center min-h-[calc(100vh-100px)]">
-	<div class="text-center prose px-6 lg:p-0 lg:prose-xl">
-		<h1>Transform PDFs Like a Pro</h1>
-		<p>
-			Step into a world where managing PDFs is as thrilling as a magician's trick! With our
-			cutting-edge app, you can merge and split PDFs effortlessly. Fuse documents into a single
-			masterpiece or break them apart with pinpoint accuracy. Turn your PDF tasks into a breeze and
-			unleash your productivity!
-		</p>
-		<button class="btn btn-primary" on:click={scrollToBottom}>Get Started</button>
-	</div>
-</section>
+<div>
+	<section class="flex items-center justify-center min-h-[calc(100vh-100px)]">
+		<div class="text-center prose lg:prose-xl">
+			<h1>Transform PDFs Like a Pro</h1>
+			<p>
+				Step into a world where managing PDFs is as thrilling as a magician's trick! With our
+				cutting-edge app, you can merge and split PDFs effortlessly. Fuse documents into a single
+				masterpiece or break them apart with pinpoint accuracy. Turn your PDF tasks into a breeze
+				and unleash your productivity!
+			</p>
+			<button class="btn btn-primary" on:click={scrollToBottom}>Get Started</button>
+		</div>
+	</section>
 
-<section class="lg:mb-24 p-6 lg:p-0">
-	<h2 class="text-2xl lg:text-4xl font-black mb-8">Our Tools</h2>
+	<section class="lg:mb-24">
+		<h2 class="text-2xl lg:text-4xl font-black mb-8">Our Tools</h2>
 
-	<div class="grid grid-cols-1 lg:grid-cols-4 gap-8">
-		{#each TOOLS as tool}
-			<a href={tool.link} class="flex-1 max-w-sm border rounded-2xl p-4 lg:p-6 hover:bg-base-200">
-				<div class="flex lg:flex-col lg:items-start items-center gap-4 mb-3">
-					<span>{@html tool.icon}</span>
-					<h3 class="font-bold text-lg">{tool.name}</h3>
-				</div>
-				<p class="opacity-80">{tool.description}</p>
-			</a>
-		{/each}
-	</div>
-</section>
+		<div class="grid grid-cols-1 lg:grid-cols-4 gap-8">
+			{#each TOOLS as tool}
+				<a href={tool.link} class="flex-1 max-w-sm border rounded-2xl p-4 lg:p-6 hover:bg-base-200">
+					<div class="flex lg:flex-col lg:items-start items-center gap-4 mb-3">
+						<span>{@html tool.icon}</span>
+						<h3 class="font-bold text-lg">{tool.name}</h3>
+					</div>
+					<p class="opacity-80">{tool.description}</p>
+				</a>
+			{/each}
+		</div>
+	</section>
+</div>
