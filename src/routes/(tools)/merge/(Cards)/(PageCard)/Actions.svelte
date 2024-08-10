36fbac0a807473c87d1dc244894f1f3941273930
Yousef Jalali <yousefjalalidev@gmail.com>
2024-08-10 14:53:20 +0300

    fix page actions on mobile

--- src/routes/(tools)/merge/(Cards)/(PageCard)/Actions.svelte
+++ src/routes/(tools)/merge/(Cards)/(PageCard)/Actions.svelte
@@ -8,33 +8,64 @@
 	export let page: Page
 	export let pageIndex: number
 
-	export let from: 'card' | 'preview' = 'card'
-
-	function showPreview() {
-		previewModal.show(pageIndex)
-	}
-
 	const dispatch = createEventDispatcher()
 
 	function onDelete() {
+		pages.deletePage(page.pageId)
 		dispatch('delete')
 	}
+
+	function onPreview() {
+		previewModal.show(pageIndex)
+		dispatch('preview')
+	}
+
+	function onRotate() {
+		pages.rotate(page.pageId, 90)
+		dispatch('rotate')
+	}
+
+	const actions = [
+		{ label: 'preview', icon: zoom, action: onPreview, dataTip: 'Preview' },
+		{
+			label: 'rotate',
+			icon: rotate,
+			action: onRotate,
+			dataTip: doc.pageCount <= 1 || doc.showPages ? 'Rotate Page' : 'Rotate Document'
+		},
+		{
+			label: 'delete',
+			icon: trash,
+			action: onDelete,
+			dataTip: doc.pageCount <= 1 || doc.showPages ? 'Delete Page' : 'Delete Document'
+		}
+	]
 </script>
 
-<div class="bg-neutral text-neutral-content shadow w-fit mx-auto join join-horizontal">
-	{#if from === 'card'}
-		<div
-			class="tooltip tooltip-bottom join-item flex-1 before:hidden lg:before:block"
-			data-tip="Preview"
-		>
-			<button
-				aria-label="preview"
-				class="btn btn-sm btn-ghost [&>svg]:size-5"
-				on:click={showPreview}
-				>{@html zoom}
-			</button>
-		</div>
-	{/if}
+{#each actions as { label, action, icon, dataTip }}
+	<li class="relative {label === 'delete' ? 'text-error' : ''} first:pt-2 last:pb-2">
+		<button on:click={action} class="absolute inset-0"></button>
+		<a href={undefined} aria-label={label} class="lg:p-1 lg:px-2">
+			{@html icon}
+			<span class="lg:hidden whitespace-nowrap">{dataTip}</span>
+		</a>
+	</li>
+{/each}
+<!-- <li>
+	<a href={undefined} aria-label="preview" on:click={showPreview} class="lg:p-1 lg:px-2">
+		{@html zoom} <span class="lg:hidden">Preview</span>
+	</a>
+</li> -->
+
+<!-- <div class="bg-neutral text-neutral-content shadow w-fit mx-auto join join-horizontal">
+	<div
+		class="tooltip tooltip-bottom join-item flex-1 before:hidden lg:before:block"
+		data-tip="Preview"
+	>
+		<button aria-label="preview" class="btn btn-sm btn-ghost [&>svg]:size-5" on:click={showPreview}
+			>{@html zoom}
+		</button>
+	</div>
 
 	<div
 		class="tooltip tooltip-bottom join-item flex-1 before:hidden lg:before:block"
@@ -59,4 +90,4 @@
 			>{@html trash}
 		</button>
 	</div>
-</div>
+</div> -->
--- src/routes/(tools)/merge/(Cards)/(PageCard)/PageCard.svelte
+++ src/routes/(tools)/merge/(Cards)/(PageCard)/PageCard.svelte
@@ -7,6 +7,8 @@
 	export let page: Page
 	export let pageIndex: number
 
+	let optionsModal: HTMLDialogElement
+
 	$: doc = $docs[page.docId]
 	$: pageNumber = $pageNum[page.pageId]
 	$: multiPages = typeof pageNumber === 'number' ? false : true
@@ -23,15 +25,18 @@
 	<div
 		class={`relative z-10 flex flex-col justify-between bg-base-200 border border-base-400 p-3 rounded-xl aspect-[180/280] ${multiPages ? 'shadow-sm' : ''}`}
 	>
+		<!-- Document Color -->
 		<div
 			class="absolute top-0 left-1/2 -translate-x-1/2 h-1 w-1/2 rounded-b-xl"
 			style="background-color: {doc.color};"
 		/>
 
+		<!-- Thumbnail -->
 		<div class="h-full flex items-center">
 			<PageCardThumbnail {page} />
 		</div>
 
+		<!-- Document name & page number in $pages -->
 		<div class="text-center text-sm flex flex-col">
 			<span class="truncate">{doc.name} </span>
 			<span class="opacity-40 text-xs mt-1">
@@ -40,10 +45,47 @@
 			</span>
 		</div>
 
-		<div
-			class="lg:hidden absolute top-0 left-1/2 -translate-x-1/2 w-fit p-2 py-3 lg:group-hover:flex mx-auto"
-		>
-			<Actions {doc} {page} {pageIndex} on:delete={() => pages.deletePage(page.pageId)} />
+		<!-- desktop -->
+		<div class="absolute top-2 left-0 w-full hidden lg:group-hover:inline-flex">
+			<ul class="bg-neutral text-neutral-content menu menu-horizontal rounded-box mx-auto">
+				<Actions {doc} {page} {pageIndex} on:delete={() => pages.deletePage(page.pageId)} />
+			</ul>
 		</div>
+
+		<!-- mobile -->
+		<button
+			class="btn btn-sm btn-ghost btn-circle bg-base-100 border border-base-300 absolute right-1 top-1 lg:hidden"
+			on:click={() => optionsModal.showModal()}
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-5"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+				/>
+			</svg>
+		</button>
+		<dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle" bind:this={optionsModal}>
+			<div class="modal-box p-0">
+				<ul class="menu p-0">
+					<Actions
+						{doc}
+						{page}
+						{pageIndex}
+						on:delete={() => optionsModal.close()}
+						on:preview={() => optionsModal.close()}
+					/>
+				</ul>
+			</div>
+			<form method="dialog" class="modal-backdrop">
+				<button></button>
+			</form>
+		</dialog>
 	</div>
 {/if}
