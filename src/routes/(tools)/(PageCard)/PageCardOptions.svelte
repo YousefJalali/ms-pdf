aff23052d97fbc1f5a5cedf35c8a0241f2d0c191
Yousef Jalali <yousefjalalidev@gmail.com>
2024-08-12 19:39:46 +0300

    fix tooltip

--- src/routes/(tools)/(PageCard)/PageCardOptions.svelte
+++ src/routes/(tools)/(PageCard)/PageCardOptions.svelte
@@ -52,7 +52,12 @@
 
 {#each actions as { label, action, icon, dataTip, dataTestId }}
 	<li class="relative {label === 'delete' ? 'text-error' : ''} ">
-		<button data-testid={dataTestId} aria-label={label} on:click={action} class="absolute inset-0">
+		<button
+			data-testid={dataTestId}
+			aria-label={label}
+			on:click={action}
+			class="absolute inset-0 z-50"
+		>
 		</button>
 		<a href={undefined} class="lg:p-1 lg:px-2">
 			<div class="lg:tooltip lg:tooltip-bottom" data-tip={dataTip}>
