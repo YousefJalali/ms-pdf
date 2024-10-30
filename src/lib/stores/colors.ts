c9875d2b43f79ba186ce963b16ab3b9d796752a5
Yousef Jalali <yousefjalalidev@gmail.com>
2024-10-30 08:41:09 +0300

    add more colors

--- src/lib/stores/colors.ts
+++ src/lib/stores/colors.ts
@@ -10,8 +10,29 @@ const COLORS = [
 	'#2F4F4F', // Dark Slate Gray
 	'#8B0000', // Dark Red
 	'#483D8B', // Dark Slate Blue
-	'#2E8B57' // Sea Green
+	'#2E8B57', // Sea Green
+	'#000080', // Navy
+	'#708090', // Slate Gray
+	'#808000', // Olive
+	'#556B2F', // Dark Olive Green
+	'#B22222', // Firebrick
+	'#5F9EA0', // Cadet Blue
+	'#228B22', // Forest Green
+	'#6A5ACD', // Slate Blue
+	'#4682B4', // Steel Blue
+	'#800080', // Purple
+	'#D2691E', // Chocolate
+	'#A52A2A', // Brown
+	'#CD5C5C', // Indian Red
+	'#FF4500', // Orange Red
+	'#FF6347', // Tomato
+	'#DAA520', // Goldenrod
+	'#B8860B', // Dark Goldenrod
+	'#FF8C00', // Dark Orange
+	'#2F4F4F', // Dark Slate Gray
+	'#DC143C' // Crimson
 ]
+
 function handleColors() {
 	const { subscribe, set, update } = writable(COLORS)
 
