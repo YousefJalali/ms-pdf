eca7fac7284a1da3544b305524fbe2fc77e49574
Yousef Jalali <yousefjalalidev@gmail.com>
2024-11-03 13:01:00 +0300

    show nav on hover in md screens

--- src/routes/(tools)/+layout.svelte
+++ src/routes/(tools)/+layout.svelte
@@ -8,7 +8,6 @@
 	<input id="nav-drawer" type="checkbox" class="drawer-toggle" bind:this={drawer} />
 
 	<!-- main content -->
-	<!-- drawer-content flex-1 lg:min-h-0 flex flex-col has-[#dropzone-file]:lg:bg-base-200 has-[#dropzone-file]:lg:rounded-box has-[#dropzone-file]:lg:p-3 -->
 	<div class="drawer-content flex-1 lg:min-h-0 flex flex-col bg-base-300">
 		<div class="flex flex-1 lg:min-h-0 gap-8 px-6 lg:p-0">
 			<slot />
@@ -16,11 +15,24 @@
 	</div>
 
 	<!-- nav -->
-	<div class="drawer-side z-50 h-full">
+	<div class="group drawer-side z-50 h-full">
 		<label for="nav-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
 
-		<div class="flex-1"><a class="btn btn-ghost btn-square" href="/">LOGO</a></div>
+		<div class="flex flex-col bg-base-100 h-full">
+			<div class="flex-1 h-0 overflow-y-scroll">
+				<div class="flex-1"><a class="btn btn-ghost btn-square" href="/">LOGO</a></div>
+				<Nav {drawer} />
+			</div>
+		</div>
 
-		<Nav {drawer} />
+		<!-- Show Nav as fixed layer on md and lg screens -->
+		<div
+			class="shadow-xl hidden xl:!hidden lg:group-has-[:hover]:flex fixed top-0 left-0 h-full flex-col bg-base-100 z-50 [&_.title]:inline-block"
+		>
+			<div class="flex-1 h-0 overflow-y-scroll">
+				<div class="flex-1"><a class="btn btn-ghost btn-square" href="/">LOGO</a></div>
+				<Nav {drawer} />
+			</div>
+		</div>
 	</div>
 </div>
--- src/routes/(tools)/Nav.svelte
+++ src/routes/(tools)/Nav.svelte
@@ -5,7 +5,7 @@
 	export let drawer
 </script>
 
-<ul class="menu bg-base-100 min-h-full xl:min-w-64 w-[70vw] md:w-64 lg:w-fit p-0 py-6 gap-2">
+<ul class="menu bg-base-100 xl:min-w-64 w-[70vw] md:w-64 lg:w-fit p-0 py-6 gap-2">
 	{#each TOOLS as tool}
 		<li>
 			<a
@@ -17,7 +17,8 @@
 				<span class="[&_*]:fill-neutral dark:[&_>*]:fill-neutral-content [&_svg]:size-5"
 					>{@html tool.icon}</span
 				>
-				<span class="lg:hidden xl:inline-block">{tool.name}</span>
+				<span data-title class="title lg:hidden xl:inline-block whitespace-nowrap">{tool.name}</span
+				>
 			</a>
 		</li>
 	{/each}
