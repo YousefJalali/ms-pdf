8b75e1f9afb4c33a62ccfa1476a21bd471b072f3
Yousef Jalali <yousefjalalidev@gmail.com>
2025-05-19 12:09:48 +0300

    replace with shadcn components

--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -1,7 +1,5 @@
-export { default as Modal } from './Modal.svelte'
 export { default as DnDFIleInput } from './DnDFIleInput.svelte'
 export { default as UploadButton } from './UploadButton.svelte'
 export { default as Alert } from './Alert.svelte'
 export { default as PageLoadingState } from './PageLoadingState.svelte'
-export { default as Popover } from './Popover.svelte'
 export * from './icons'
